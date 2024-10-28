32272e3b3fac4e412de359ef4e5edb96c67cefec
Adam Lane <enalmada@gmail.com>
2024-10-27 20:53:05 -0700

    dependency updates (#57)

--- build.ts
+++ build.ts
@@ -1,19 +1,19 @@
 // bun types conflicts with dom types so just putting it where it is needed
 /// <reference types="bun-types" />
 
-import { bunBuild, getSourceFiles } from '@enalmada/bun-externals';
+import { bunBuild, getSourceFiles } from "@enalmada/bun-externals";
 
 async function buildWithExternals(): Promise<void> {
-  const entrypoints = await getSourceFiles();
+	const entrypoints = await getSourceFiles();
 
-  // bunBuild handles build failure
-  await bunBuild({
-    entrypoints,
-    outdir: './dist',
-    target: 'node',
-    external: ['*'],
-    root: './src',
-  });
+	// bunBuild handles build failure
+	await bunBuild({
+		entrypoints,
+		outdir: "./dist",
+		target: "node",
+		external: ["*"],
+		root: "./src",
+	});
 }
 
 void buildWithExternals();
--- website/astro.config.mjs
+++ website/astro.config.mjs
@@ -1,59 +1,59 @@
-import prefetch from '@astrojs/prefetch';
-import react from '@astrojs/react';
-import starlight from '@astrojs/starlight';
-import tailwind from '@astrojs/tailwind';
-import { defineConfig } from 'astro/config';
+import prefetch from "@astrojs/prefetch";
+import react from "@astrojs/react";
+import starlight from "@astrojs/starlight";
+import tailwind from "@astrojs/tailwind";
+import { defineConfig } from "astro/config";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [
-    starlight({
-      title: 'npm-module-template',
-      defaultLocale: 'root', // optional
-      locales: {
-        root: {
-          label: 'English',
-          lang: 'en', // lang is required for root locales
-        },
-      },
-      social: {
-        github: 'https://github.com/Enalmada/npm-module-template',
-      },
-      sidebar: [
-        {
-          label: 'Getting Started',
-          items: [
-            {
-              label: 'Getting Started',
-              link: '/guides/getting-started/',
-            },
-            {
-              label: 'Deploy Website',
-              link: '/guides/website/',
-            },
-          ],
-        },
-        {
-          label: 'Technologies',
-          items: [
-            {
-              label: 'Summary',
-              link: '/technologies/summary/',
-            },
-            {
-              label: 'Build',
-              link: '/technologies/build/',
-            },
-          ],
-        },
-      ],
-      customCss: ['./src/assets/landing.css', './src/tailwind.css'],
-    }),
-    react(),
-    // applyBaseStyles causes lists to not work anymore
-    tailwind({
-      applyBaseStyles: false,
-    }),
-    prefetch(),
-  ],
+	integrations: [
+		starlight({
+			title: "npm-module-template",
+			defaultLocale: "root", // optional
+			locales: {
+				root: {
+					label: "English",
+					lang: "en", // lang is required for root locales
+				},
+			},
+			social: {
+				github: "https://github.com/Enalmada/npm-module-template",
+			},
+			sidebar: [
+				{
+					label: "Getting Started",
+					items: [
+						{
+							label: "Getting Started",
+							link: "/guides/getting-started/",
+						},
+						{
+							label: "Deploy Website",
+							link: "/guides/website/",
+						},
+					],
+				},
+				{
+					label: "Technologies",
+					items: [
+						{
+							label: "Summary",
+							link: "/technologies/summary/",
+						},
+						{
+							label: "Build",
+							link: "/technologies/build/",
+						},
+					],
+				},
+			],
+			customCss: ["./src/assets/landing.css", "./src/tailwind.css"],
+		}),
+		react(),
+		// applyBaseStyles causes lists to not work anymore
+		tailwind({
+			applyBaseStyles: false,
+		}),
+		prefetch(),
+	],
 });
